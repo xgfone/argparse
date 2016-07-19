94a93ed06933e9aeb773c93ee1539c3cbc97c538
xgfone <xgfone@126.com>
2016-07-19 09:41:06 +0800

    Add the validation to validate whether the string is empty

--- valid.go
+++ valid.go
@@ -51,4 +51,22 @@ func (m mValid) validate(tag reflect.StructTag, value interface{}) error {
 
 func init() {
 	RegisterValidFunc("validate_num_range", ValidateNumberRange)
+	RegisterValidFunc("validate_str_not_empty", ValidateStrNotEmpty)
+}
+
+// Validate whether the value is a non-empty string.
+//
+// Return nil if it's not empty. Or false. If the value is the type of string,
+// don't validate it, that's, return nil.
+//
+// This function has been registered as "validate_str_not_empty", and you can
+// use it with the tag of `validate:"validate_str_not_empty"`.
+func ValidateStrNotEmpty(tag string, value interface{}) error {
+	if v, ok := value.(string); !ok {
+		return nil
+	} else if strings.TrimSpace(v) == "" {
+		return errors.New("The string is empty")
+	} else {
+		return nil
+	}
 }
